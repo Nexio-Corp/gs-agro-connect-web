a7deace3bc4aec2481833902337a8d0d832fe3ba
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-06-07 19:51:06 -0300

    fixed time bug

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -1,5 +1,5 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks){
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
@@ -7,25 +7,27 @@ class MobileNavbar {
         this.handleClick = this.handleClick.bind(this);
     }
 
-    animateLinks(){
+    animateLinks() {
         this.navLinks.forEach((link, index) => {
-            link.style.animation 
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`)
-        })
+            const timeAnimation =
+                index < 6 ? index / 7 + 0.1 : (index - 6) / 7 + 0.1; // Caso esteja no sexto item já está na segunda lista, então deve-se subtrair 6 do index para que o tempo de animação seja o mesmo
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${timeAnimation}s`);
+        });
     }
-    handleClick(){
+    handleClick() {
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
     }
 
-    addClickEvent(){
+    addClickEvent() {
         this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
-    init(){
-        if(this.mobileMenu){
+    init() {
+        if (this.mobileMenu) {
             this.addClickEvent();
         }
         return this;
@@ -35,7 +37,7 @@ class MobileNavbar {
 const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
     ".lista-nav",
-    ".lista-nav li",
-    );
+    ".lista-nav li"
+);
 
 mobileNavbar.init();
